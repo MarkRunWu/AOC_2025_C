ec369d35d260b9c0589064c7b128a9b2f9088a86
mark wu <saybyebyet@gmail.com>
2025-12-02 23:06:56 +0800

    Enhance output format for day1

--- day1/day1.c
+++ day1/day1.c
@@ -15,20 +15,17 @@ Movement move(int current_pos, char* movement) {
         next_steps = -steps;
     }
     int end_pos = current_pos + next_steps;
+    int pass_zero_count = 0;
     while(end_pos < 0) {
         end_pos += 100;
+        pass_zero_count++;
     }
     while(end_pos >= 100) {
         end_pos -= 100;
-    }
-    if (current_pos == 0 ) {
-        steps -= *movement == 'L' ? 100 - end_pos : end_pos;
-    }
-    int target_pos = end_pos + steps;
-    int pass_zero_count;
-    while(target_pos >= 100) {
-        target_pos -= 100;
         pass_zero_count++;
+    }
+    if ((current_pos == 0 || end_pos == 0) && pass_zero_count > 0) {
+        pass_zero_count--;
     }
 
     Movement m = { 
@@ -51,21 +48,23 @@ int main(int argc, char** argv) {
     char text[100];
     int current_pos = 50;
     int password = 0;
-    Movement pre = {};
+    printf("The dial starts by pointing at %d\n", current_pos);
     while(fgets(buffer, sizeof(buffer), fp)) {
-        Movement m = move(current_pos, buffer);
-        if (m.next_pos == 0) {
-            password++;
+        char* b = buffer;
+        while(*b != '\n') {
+            b++;
         }
+        *b = 0;
+
+        Movement m = move(current_pos, buffer);
         password += m.pass_zero_count;
 
-        pre = m;
-        int offset = sprintf(text, "%s %d to %d", buffer, current_pos, m.next_pos);
+        int offset = sprintf(text, "The dial is rotated %s to point at %d", buffer, m.next_pos);
         if (m.pass_zero_count > 0) {
-            offset += sprintf(text + offset, ", dial through zero count: %d", m.pass_zero_count);
+            offset += sprintf(text + offset, ", during this rotation, it points at 0 (%d) times", m.pass_zero_count);
         }
         if (m.next_pos == 0) {
-            offset += sprintf(text + offset, ", stays at zero");
+            password++;
         }
         printf("%s\n", text);
         current_pos = m.next_pos;
